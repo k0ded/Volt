8ab84d1fec1a60a6b9f66c2c6a79dab509d66be8
Ivar Jönsson <chunktreasure@gmail.com>
2025-06-29 12:44:33 +0200

    Fixed asset property not getting a correct ID

--- Engine/Source/Sandbox/Private/Sandbox/Utility/EditorUtilities.cpp
+++ Engine/Source/Sandbox/Private/Sandbox/Utility/EditorUtilities.cpp
@@ -47,7 +47,7 @@ bool EditorUtils::Property(const std::string& text, Volt::AssetHandle& assetHand
 		}
 	}
 
-	std::string textId = "##" + std::to_string(UI::GetAndIncrementStackID());
+	std::string textId = UI::MakePropertyID();
 
 	changed = UI::DrawItem(ImGui::GetColumnWidth() - 2.f * 25.f, [&]()
 	{
@@ -69,7 +69,7 @@ bool EditorUtils::Property(const std::string& text, Volt::AssetHandle& assetHand
 
 	ImGui::SameLine();
 
-	std::string buttonId = "X##" + std::to_string(UI::GetAndIncrementStackID());
+	std::string buttonId = "X" + UI::MakePropertyID();
 	if (ImGui::Button(buttonId.c_str(), { 24.5f, 24.5f }))
 	{
 		assetHandle = Volt::Asset::Null();
@@ -78,8 +78,8 @@ bool EditorUtils::Property(const std::string& text, Volt::AssetHandle& assetHand
 
 	ImGui::SameLine();
 
-	std::string selectButtonId = "...##" + std::to_string(UI::GetAndIncrementStackID());
-	std::string popupId = "AssetsPopup##" + text + std::to_string(UI::GetAndIncrementStackID());
+	std::string selectButtonId = "..." + UI::MakePropertyID();
+	std::string popupId = "AssetsPopup" + UI::MakePropertyID();
 	const bool startState = s_assetBrowserPopupsOpen[popupId].state;
 
 	if (ImGui::Button(selectButtonId.c_str(), { 24.5f, 24.5f }))
