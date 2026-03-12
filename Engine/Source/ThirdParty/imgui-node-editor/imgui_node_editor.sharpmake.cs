b1055cfe07b02e3a1b85a322d1e97c0cd2faa930
Cooltomten <Cooltomten.fr@gmail.com>
2026-03-12 17:56:14 +0100

    Fix a build script mistake

--- Engine/Source/ThirdParty/imgui-node-editor/imgui_node_editor.sharpmake.cs
+++ Engine/Source/ThirdParty/imgui-node-editor/imgui_node_editor.sharpmake.cs
@@ -23,7 +23,6 @@ namespace VoltSharpmake
                 "imgui_bezier_math.h",
                 "imgui_bezier_math.inl",
                 "imgui_canvas.cpp",
-                <Volt_imgui_extras/imgui_canvas.h>,
                 "imgui_extra_math.h",
                 "imgui_extra_math.inl",
                 "imgui_node_editor.cpp",
